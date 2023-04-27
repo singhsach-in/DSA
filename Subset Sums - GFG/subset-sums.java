3ef78707d20fd4569c4ee2caae9b340100472da8
Sachin Singh Rajpoot <98595529+singhsach-in@users.noreply.github.com>
2023-04-27 12:02:52 +0530

    Added solution - LeetHub

--- Subset Sums - GFG/subset-sums.java
+++ Subset Sums - GFG/subset-sums.java
@@ -34,27 +34,22 @@ class GFG
 //User function Template for Java//User function Template for Java
 class Solution{
     ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N){
-        ArrayList<Integer> al = new ArrayList<>();
         ArrayList<Integer> sumAl = new ArrayList<>();
         int sum=0;
         int index=0;
         int n = arr.size();
-        fun(index, n, sum, al, arr, sumAl);
+        fun(index, n, sum, arr, sumAl);
         return sumAl;
     }
-    static void fun(int index, int n, int sum, ArrayList<Integer> al, ArrayList<Integer> arr,  ArrayList<Integer> sumAl) {
+    static void fun(int index, int n, int sum, ArrayList<Integer> arr,  ArrayList<Integer> sumAl) {
         
         if(index == n){
-            // System.out.println(sum);
             sumAl.add(sum); 
             return;
         }
         
-        al.add(arr.get(index));
-        sum += arr.get(index);
-        fun(index+1, n, sum, al, arr, sumAl);
-        al.remove(al.size() - 1);
-        sum -= arr.get(index);
-        fun(index+1, n, sum, al, arr, sumAl);
+        fun(index+1, n, sum+arr.get(index),  arr, sumAl);
+       
+        fun(index+1, n, sum, arr, sumAl);
     }
 }
