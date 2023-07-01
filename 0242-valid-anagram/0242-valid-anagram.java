2ccbd34958e8a2919b2fe0bad30483eb39a67c82
Sachin Singh Rajpoot <98595529+singhsach-in@users.noreply.github.com>
2023-07-01 19:41:40 +0530

    Time: 2 ms (99.79%), Space: 43.7 MB (39.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,19 +2,12 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         
         if(s.length() != t.length()) return false;
-        int[] frq1 = new int[5*10000];
-        int[] frq2 = new int[5*10000];
+        int[] frq = new int[26];
+        for(char x : s.toCharArray()) frq[x - 'a']++;
+        for(char x : t.toCharArray()) frq[x - 'a']--;
         
-        for(int i=0; i<s.length(); i++) {
-            frq1[s.charAt(i) - '0']++;
-            frq2[t.charAt(i) - '0']++;
-        }
+        for(int x : frq) if( x != 0 ) return false;
         
-        for(int i=0; i<s.length(); i++){
-            if(frq1[s.charAt(i) - '0'] != frq2[s.charAt(i) - '0']){
-                return false;
-            }
-        }
         return true;
     }
 }
