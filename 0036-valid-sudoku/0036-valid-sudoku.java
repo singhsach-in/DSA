71c2dfecd5cc0b835ea4d5e3be3e160afa8b8635
Sachin Singh Rajpoot <98595529+singhsach-in@users.noreply.github.com>
2023-07-03 12:24:47 +0530

    Time: 13 ms (32.65%), Space: 44.2 MB (12.74%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,61 +1,21 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        // checking row
-        for(int i=0; i<board.length; i++) {
-            if(!chkRow(board[i])) return false;
-        }
-        // checking Column
-        if(!chkCol(board)) return false;
+        Set<String> st = new HashSet<>();
         
-        // checking sub cube
-        for(int i=0; i<9; i+=3){
-            for(int j=0; j<9; j+=3){
-                if(!chkSubCube(i, j, board)) return false;
+
+        for(int row=0; row<9; row++) {
+            for(int col=0; col<9; col++) {
+                char num = board[row][col];
+                if(num != '.'){
+                    if(!st.add(num + "in row" + row) ||
+                       !st.add(num + "in col" + col) ||
+                       !st.add(num + "in cube" + (row/3)+ ","+ (col/3))) {
+                        return false;
+                    }
             }
-        }
-        
-        return true;
-        
-    }
-    
-    public static boolean chkRow(char[] arr) {
-        int[] frq = new int[10];
-        for(char x : arr){
-            if(x != '.' && x != '\u0000') {
-                int k = x - '0';
-                System.out.println(x +" "+k);
-                frq[k]++;
-                if(frq[k] >= 2) return false;
-            }
-            
-        }
-        return true;
-    }
-    public static boolean chkCol(char[][] arr) {
-        // int[][] frq = new int[10][10];
-        char[] arr2 = new char[9];
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++) {
-                // if(arr[j][i] != '.')
-                arr2[j] = arr[j][i];
-            }
-            if(!chkRow(arr2)) return false;
-        }
-        
-        return true;
-    }
-    public static boolean chkSubCube(int a, int b, char[][] arr){
-        int[] frq = new int[10];
-        for(int i=a; i<a+3; i++) {
-            for(int j=b; j<b+3; j++){
-                if(arr[i][j] != '.' && arr[i][j] != '\u0000') {
-                    int k = arr[i][j] - '0';
-                    frq[k]++;
-                    if(frq[k] >= 2) return false;
                 }
                 
-            }
         }
-            return true;
+        return true;
     }
 }
